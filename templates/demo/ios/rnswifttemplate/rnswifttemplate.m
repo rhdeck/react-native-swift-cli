b8ffc89d9b0b12e57b12473224fa08a1e71cfdbe
Ray Deck <ray@raydeck.com>
2017-11-15 13:34:13 -0500

    Fixing voidDemo bridge to match type and name

--- templates/demo/ios/rnswifttemplate/rnswifttemplate.m
+++ templates/demo/ios/rnswifttemplate/rnswifttemplate.m
@@ -3,7 +3,7 @@
 
 @interface RCT_EXTERN_MODULE(rnswifttemplate, NSObject)
 RCT_EXTERN_METHOD(demo:(NSString *)name success:(RCTPromiseResolveBlock)success reject:(RCTPromiseRejectBlock)reject);
-RCT_EXTERN_METHOD(voidDemo:(NSString *)name);
+RCT_EXTERN_METHOD(voidDemo:(NSDictionary *)dic);
 RCT_EXTERN_METHOD(delayedSend:(NSString *)message ms:(int)ms);
 @end
 
